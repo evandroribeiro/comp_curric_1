53b8fde49c700419b1a58c0d9b2cb42ce80fca7c
Evandro Ribeiro <mano-xxe@hotmail.com>
2012-11-14 20:16:30 -0200

    movidos arquivos para a pasta exercicios

---
+++ exerc_3.php
@@ -0,0 +1,40 @@
+<!docty pe html>
+<html>
+<head>
+<meta charset = "UTF 8"/>
+</head>
+<body>
+<?php
+//3.	Utilizando a condição switch(): 
+//a) Caso a $qtd de produtos for zero: mostrar uma mensagem informando que não possui nenhum produto cadastrado;
+//b) Caso a $qtd for 1: mostrar a mensagem: "Você possui um produto cadastrado";
+//c) Caso a $qtd for 1, 2 e 3: Mostrar a mensagem: "Você possui vários produtos cadastrados";
+//d) Caso não for nenhum desses números, mostrar: "Cadastro inválido, tente novamente";
+
+
+   #produtos
+
+    $produtos=3;
+     
+    
+    switch($produtos){ 
+          
+         case 0:
+           echo "nao posui produtos cadastrado";
+           break;
+           case 1:
+             echo "você possui 1 produto cadastrado";
+             break;
+             case 2:
+               echo "você possui 2 produtos cadastrados";
+               break;
+               case 3:
+                 echo "você possui varios produtos cadastrados";
+                 break;
+                   
+                          default :
+                          echo "cadastro invalido";
+                      }
+         ?>                 
+</body>
+</html>	    
---
+++ exerc_4.php
@@ -0,0 +1,49 @@
+<html>
+<?php
+//4.	Utilizando o for() e while():
+//a) Mostrar 25x a frase: "Lactobacilos vivos também possuem sentimentos";
+//b) Escrever um algoritmo que receba qualquer valor e mostre os valores de 0 até o número digitado. Ex: $var = 3; "0 1 2 3";
+//c) capturar dois valores em variáveis e mostrar os números entre eles. Ex: 5 e 10, imprimir: "6 7 8 9";
+//d) Escrever um algoritmo que multiplique os números utilizando apenas o operador +, ex: (3 * 5) = 5 + 5 + 5
+
+$linha="Lactobacilos vivos também possuem sentimentos"
+
+?>
+<html>
+  <head>
+    <title>Teste PHP</title>
+      <meta charset = "UTF 8"/>
+        </head>
+<body>
+<?php
+
+for ($i = 0; $i < 26; $i++)
+{
+echo "$linha $i <br>";
+}
+
+?>
+
+<?php
+  $n=5;
+   for( $i=0;$i<=$n;$i++){
+   	    echo "$i=$n-1 <br>";
+   	     }
+?>
+
+<?php
+ $n=10;
+    for( $i=5;$i<=$n;$i++){
+   	    echo "$i=$n-1 <br><t>";
+   	     }
+  ?>  
+  <?php
+      $n1=5;
+      $n=4;
+      echo "($n1*$n)=$n."+".$n";
+
+
+
+  ?> 
+</body>
+</html>S
