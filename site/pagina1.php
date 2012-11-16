96c919a2d670325769a7873f6f46bb3e7b83cd70
Evandro Ribeiro <mano-xxe@hotmail.com>
2012-11-15 23:03:48 -0200

    modificado exerc_4.php exerc_5.php exerc_6.php

---
+++ site/pagina1.php
@@ -0,0 +1,38 @@
+<?php
+     //variaveis
+
+
+
+?>       
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset = "UTF-8">
+	 <title> 
+	    <?php	
+	                     //titulo e sobre titulo
+
+	       echo $titulo."-".$subtitulo;
+         ?>
+          </title>
+         
+  </head>             
+<body>
+	    <?php	
+	     ?>
+         <h1>exercicio 2</h1>
+         <?php
+
+                                //programam
+  
+
+          </div>	     
+             <!--DIV #rodape-->
+             <div  id="rodape">
+         	 &reg; "COPYright Site.com "
+        <?php  
+           echo date("Y");
+        ?>
+     </div>   
+</body>
+</html>	          	
