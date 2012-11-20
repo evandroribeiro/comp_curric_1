a495846c5fcd47bc67ff593a71a72d8e4bcae145
Evandro Ribeiro <mano-xxe@hotmail.com>
2012-11-20 20:03:27 -0200

    modificado contact.php css/bootstranp.css pagina1.php

---
+++ site/contact.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="formulario.php" metod="POST">
+
+	<fieldset>
+        <legend> formulario de contatos </legend>
+
+         <p>
+          <label for="id nome">NOME:</label><br>
+           <input type="texto nome"></p>
+
+            <p>
+              <form>
+            <label for="id imail">IMAIL:</label><br>
+              <input type="email">
+          
+               </form></p>
+
+                
+               <p>
+               <label for="id mensagem">MENSAGEM:</label><br>
+                <textarea rows="texto mensagem"></textarea></p>
+
+                <p>
+  <button type="submet" class="btn btn-danger">OK</button>
+</p>
+
+</fieldset>
+</body>
+</html>
+
+
+//$_POST = variavel
+//var_dumb($_POST);
+
+
+//se for enviado
+
+if (isset($POST["pagina"]))
--- site/pagina1.php
+++ site/pagina1.php
@@ -1,38 +1,125 @@
-<?php
-     //variaveis
-
-
-
-?>       
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
   <head>
-  	<meta charset = "UTF-8">
-	 <title> 
-	    <?php	
-	                     //titulo e sobre titulo
+    <meta charset="utf-8">
+    <title>site mano</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="evandro luis ribeiro mano_xxe@hotmail.com">
 
-	       echo $titulo."-".$subtitulo;
-         ?>
-          </title>
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+   
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
          
-  </head>             
-<body>
-	    <?php	
-	     ?>
-         <h1>exercicio 2</h1>
-         <?php
+          <a class="brand" href="#">Projeto prof:Tiago Link</a>
+          <div class="nav-collapse collapse">
+            
+            <ul class="nav">
+              <li class="active"><a href="#">pagina1</a></li>
+              <li class=><a href="#">pagina2.</a></li>
+              <li class=><a href="#">sobre mim.</a></li>
+              <li><a href="#contact">Contato</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Sidebar</li>
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <div class="span9">
+          <div class="hero-unit">
+            <h1>Ola, mundo!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+          </div>
+          <div class="row-fluid">
+            <div class="span4">
+              <h2>titulo 2</h2>
+             <p> Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
 
-                                //programam
-  
+<p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
 
-          </div>	     
-             <!--DIV #rodape-->
-             <div  id="rodape">
-         	 &reg; "COPYright Site.com "
-        <?php  
-           echo date("Y");
-        ?>
-     </div>   
-</body>
-</html>	          	
+<p>Casamentiss faiz malandris se pirulitá, Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer Ispecialista im mé intende tudis nuam golada, vinho, uiski, carirí, rum da jamaikis, só num pode ser mijis. Adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+
+
+
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              
+            </div><!--/span-->
+          </div><!--/row-->
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
